f9ac789bbda9722b0eb62c38af1b5442d309a1a7
Alex <alex.pigalyov@gmail.com>
2021-07-05 11:52:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpamTelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
